d1ab24b386baf605bb4ad309e932b2a3619dbb34
Eugenio Mattei <zolla@aloy.meoem.woo>
2020-03-15 16:46:00 -0400

    - Fixed typo and bug on folder checking

--- config_scripts/bash_setup.sh
+++ config_scripts/bash_setup.sh
@@ -1,7 +1,7 @@
 # Make backup of existing .bashrc, .bash_aliases, .dir_colors
 [ -e ${HOME}/.bashrc ] && mv ${HOME}/.bashrc ${HOME}/.bashrc.bak
 [ -e ${HOME}/.bash_aliases ] && mv ${HOME}/.bash_aliases ${HOME}/.bash_aliases.bak
-[ -e ${HOME}/.dir_colorsi ] && mv ${HOME}/.dir_colors ${HOME}/.dir_colors.bak
+[ -e ${HOME}/.dir_colors ] && mv ${HOME}/.dir_colors ${HOME}/.dir_colors.bak
 
 # Link the bash config files to the new location
 ln -s $( readlink -f ../bash/.bashrc ) ${HOME}/.bashrc
@@ -9,5 +9,5 @@ ln -s $( readlink -f ../bash/.bash_aliases ) ${HOME}/.bash_aliases
 ln -s $( readlink -f ../bash/.dir_colors.256dark ) ${HOME}/.dir_colors
 
 # Add the new scripts to path
-[! -d ${HOME}/.local/bin ] && mkdir ${HOME}/.local/bin
+[ ! -d ${HOME}/.local/bin ] && mkdir ${HOME}/.local/bin
 ln -s $( readlink -f ../bin/* ) ~/.local/bin/
--- config_scripts/nvim_setup_with_dein.sh
+++ config_scripts/nvim_setup_with_dein.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Check if nvim folder exists; if not, create it.
-[! -d ${XDG_CONFIG_HOME}/nvim ] && mkdir -P ${XDG_CONFIG_HOME}/nvim
+[ ! -d ${XDG_CONFIG_HOME}/nvim ] && mkdir -p ${XDG_CONFIG_HOME}/nvim
 
 # Link the nvim config to the new location
 ln -s $( readlink -f ../nvim/*.vim ) $XDG_CONFIG_HOME/nvim/
 
 # Check if dein (nvim plugin manager) folder exists; if not, create it.
-[! -d ~/.local/share/dein ] && mkdir -P ~/.local/share/dein
+[ ! -d ~/.local/share/dein ] && mkdir -p ~/.local/share/dein
 
 # Download dein and install it in the create folder
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
@@ -23,3 +23,9 @@ rm ./installer.sh
 pip3 install --user jedi flake8 autopep8
 
 bash ./yarn_installer.sh
+
+# Fix the missing fonts for the shell
+# It still needs to be set up manually
+[ ! -d ~/.local/share/fonts ] && mkdir -p ~/.local/share/fonts
+wget https://github.com/powerline/fonts/raw/master/FiraMono/FuraMono-Medium%20Powerline.otf -P ~/.local/share/fonts/
+fc-cache -f ~/.local/share/fonts
